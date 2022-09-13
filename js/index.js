512dc239d9a8fa81bab9ed56bb71f41b3ed85ed0
Cengizhan Anıl Onay <cengizhananilonay@gmail.com>
2022-09-14 00:04:06 +0300

    vanillaJsToDoV1

--- js/index.js
+++ js/index.js
@@ -1,75 +1,150 @@
-    
-    document.addEventListener('DOMContentLoaded', function () {
-    username = localStorage["username"];
-    if(!localStorage["tasks"]){
-        localStorage["tasks"] = "{}";
-    }
+    const form = document.querySelector('form');
+    const input = document.querySelector("#taskAdderInput");
+    const btnDeleteAll =document.querySelector("#btnDeleteAll");
+    const taskList = document.querySelector("#taskList");
   
-    })
 
-    
-    
-    const task_adder = document.querySelector(".task_adder");
-    const task_list = document.querySelector("ul#tasks");
-   
-    function add_task_event(){
-        var count = document.querySelector("#tasks").children.length+1;
-        var val = document.querySelector("input[name=task_adder]").value;
-        var error_message_area = document.querySelector(".error_message_area");
+        loadItems();
+        eventListeners();
+  
 
-        if(val==""){
-            error_message_area.classList.remove("d-none");
-            error_message_area.innerHTML=`<i class="fa fa-exclamation-triangle pe-3" aria-hidden="true"></i> Sence Boş Olabilir Mi ?`;
-            setTimeout(() => {
-            error_message_area.classList.add("d-none");
-            error_message_area.innerHTML=``;
-            }, 3000);
+    function eventListeners(){
+        form.addEventListener('submit',addNewItem);
+        taskList.addEventListener("click",deleteItem);
+        btnDeleteAll.addEventListener("click",deleteAllTasks);
+    }
+
+    // Get Data From Local Storage
+    function getItemsFromLS(){
+        if(localStorage["items"]==null){
+            items = [];
+        }else{
+            items = JSON.parse(localStorage["items"]);
+        }
+        return items;
+    }
+    // Edit Item From Local Storage
+    function setItemLS(text){
+        items = getItemsFromLS();
+        items.push(text);
+        localStorage["items"]=JSON.stringify(items);
+    }
+    // Delete Item From Local Storage
+    function deleteItemLS(text){
+        var val = text;
+        items = getItemsFromLS();
+
+        items.forEach(function(item,index,val){
+            if(item==text){
+                items.splice(index,1);
+            }
+            index++;
+        })
+        localStorage["items"] = JSON.stringify(items);
+    }
+    // Load Items
+    function loadItems(){
+        items=getItemsFromLS();
+        items.forEach(function(item){
+            createItem(item);
+        });
+    }
+    // Create Task Item
+    function createItem(text){
+        var val = text;
+        const li = document.createElement("li");
+        li.className="task-item d-flex";
+        li.innerHTML=`<input type="text" class="form-custom-control me-3" name="task-" disabled="disabled" value="${val}">
+                      <i class="fa-regular fa-pen-to-square data-editor" data-editable="false"></i>
+                      <i class="fa-solid fa-trash text-danger"></i>
+        `;
+        taskList.appendChild(li);
+
+    }
+    // Add New Task
+    function addNewItem(e){
+        e.preventDefault();
+            var text = input.value;
+            input.value="";
+            // Create Item
+             createItem(text);
+             // Save to local storage
+             setItemLS(text);
+    }
+    var old_text;
+    // Delete Task
+    function deleteItem(e){
+
+        if(e.target.className=="fa-solid fa-trash text-danger"){
+            e.target.parentElement.remove();
+            
+            // Delete From Local Storage
+            deleteItemLS(e.target.parentElement.querySelector("input").value);
+        }
+        // Change item to edit mode
+        else if(e.target.className=="fa-regular fa-pen-to-square data-editor"){
+             e.target.className="far fa-save text-warning data-editor";
+             old_text = e.target.parentElement.querySelector("input").value;
+             changeEditable(e.target,old_text);
+
+        }
+        // İf task in edit mode
+        else if(e.target.getAttribute("data-editable")){
+         changeEditable(e.target,old_text);
         }
         else{
-            var li = document.createElement("li");
-            li.classList.add("task-item","d-flex");
-            li.name=`task-${count}`;
-            li.innerHTML=`
-            <b>${count})</b>
-            <input type="text" class="form-custom-control me-3" name="task-${count}" disabled value="${val}">
-            <a href="javascript:" onClick="" class="btn btn-warning text-white me-2 rounded-0">
-                <i class="fa-regular fa-pen-to-square"></i>
-            </a>
-            <a href="javascript:" onClick="delete_task_event(this)" class="btn btn-danger text-white me-2 rounded-0">
-                <i class="fa-solid fa-trash"></i>
-            </a>
-            `;
-        
-            task_list.appendChild(li);    
-      
+            
+        }
 
-            var tasks = JSON.parse(localStorage["tasks"]);
-            var task_group = {};
-            for(var i in tasks){
-                task_group.push(tasks[i]);
+        e.preventDefault();
+    }
+    function changeEditable(item,old_text){
+        var input = item.parentElement.querySelector("input");
+        var edit_icons = Array.from(document.getElementsByClassName("data-editor"));
+
+        // All task change non-editable
+        edit_icons.forEach(function(e){
+            e.className="fa-regular fa-pen-to-square data-editor";
+            e.setAttribute("data-editor",false);
+            e.parentElement.querySelector("input").setAttribute("disabled",true);
+        });
+        // Chande editable our task
+        item.className=" far fa-save text-warning data-editor";
+        item.setAttribute("data-editor",true);
+        input.setAttribute("disabled",false);
+
+        // Data Editable Control
+        if(item.getAttribute("data-editable")=="false"){
+            input.removeAttribute("disabled");
+            item.setAttribute("data-editable",true);
+
+        }
+        else{
+            editItem(old_text,input.value,item);
+        }
+        
+    }
+    function editItem(old_text,text,e){
+
+        items = JSON.parse(localStorage["items"]);
+        var empty_items=[];
+        items.forEach(function(item){
+            if(item==old_text){
+                item=text;
             }
+            empty_items.push(item);
+        })
+        e.setAttribute("data-editable",false);
+        e.className="fa-regular fa-pen-to-square data-editor";
+        localStorage["items"] = JSON.stringify(empty_items);
 
-            console.log(task_group);
+       
 
 
 
-        
-            document.querySelector("input[name=task_adder]").value ="";
-            document.querySelector("input[name=task_adder]").focus();
-
-        }
     }
-
-    function delete_task_event(task){
-        var count = document.querySelector("#tasks").children.length+1;
-        if(count==3){
-            document.querySelector(".empty-message").innerHTML="";
-        }
-        console.log(count);
-        task.parentNode.remove();
+    // Delete All Tasks
+    function deleteAllTasks(e){
+        taskList.innerHTML="";
     }
-
-    document.querySelector(".task_adder").addEventListener("click",function(){
-        add_task_event();
-    });
-
+    
